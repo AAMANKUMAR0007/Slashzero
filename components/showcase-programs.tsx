6943fee7a6eb16e983434f85a66ebbc238a52460
Aaman <chaamankumar1231@gmail.com>
2025-05-01 07:49:03 +0530

    ui import changes in all components

--- components/showcase-programs.tsx
+++ components/showcase-programs.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link"
 import Image from "next/image"
-import { Button } from "@/components/ui/button"
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Badge } from "@/components/ui/badge"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import { Badge } from "./ui/badge"
+import { Button } from "./ui/button"
+
 
 export default function ShowcasePrograms() {
   const programs = [
